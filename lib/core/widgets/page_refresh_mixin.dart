39dfb46c1ba36a2e679a7a99fa4437cd1fb05a39
OneCCY <2450923084@qq.com>
2026-06-15 20:58:45 +0800

    fix: correct import path in page_refresh_mixin

--- lib/core/widgets/page_refresh_mixin.dart
+++ lib/core/widgets/page_refresh_mixin.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../../../config/routes/app_router.dart';
+import '../../config/routes/app_router.dart';
 
 /// 页面数据自动刷新 Mixin
 ///
